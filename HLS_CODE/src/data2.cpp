f7a79b5382d9b8b713bf9924d8be0532900c86cf
weiwenjiang <jiang.wwen@gmail.com>
2020-08-31 10:00:37 -0400

    hls mod

--- HLS_CODE/src/data2.cpp
+++
@@ -1,6 +0,0 @@
-#include "source_fix.h"
-
-FPGA_DATA_FIX input[N*R*C];
-FPGA_DATA_FIX weight[M*N*K*K];
-FPGA_DATA_FIX bias[M];
-FPGA_DATA_FIX output[M*(R-K+1)*(C-K+1)];
--- HLS_CODE/src/tb.cpp
+++
@@ -1,9 +0,0 @@
-int cconv_main();
-int dconv_main();
-int bn_main();
-int bilinear_main();
-
-int main2(){
-	cconv_main();
-	return 0;
-}
